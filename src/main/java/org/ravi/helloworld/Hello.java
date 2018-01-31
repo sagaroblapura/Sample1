42ffef5b5ef561112dd7698e22a17d77e6f4926c
kusiya <kusiya.farheen@accenture.com>
2018-01-31 10:04:27 +0530

    File modified to testsonar

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -3,8 +3,12 @@ package org.ravi.helloworld;
 public class Hello {
 	
 	public String displayHelloMessage(String message){
-		message = "hi welcome back";
-		return message;
+		String msg;
+		if(message != null && !message.isEmpty()){
+			msg = message;
+		}
+		else { msg = "hi";}
+		return msg;
 	}
 
 }
